835c5f34e469da3e9866e5479fc57dad2c3fb1e3
Sylvain Viollon <thefunny@gmail.com>
2010-01-07 15:09:36 +0000

    Update metadata information.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2002-2009 Infrae. All rights reserved.
+# Copyright (c) 2010 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id$
 
 from setuptools import setup, find_packages
 import os
 
-version = '2.2b1dev'
+version = '1.0dev'
 
-setup(name='silva.core.conf',
+setup(name='silva.app.redirectlink',
       version=version,
-      description="Configuration machinery for Silva",
+      description="A link which does a permanent HTTP redirect to a Silva Object",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
@@ -19,24 +19,19 @@ setup(name='silva.core.conf',
               "Programming Language :: Python",
               "Topic :: Software Development :: Libraries :: Python Modules",
               ],
-      keywords='silva core configuration',
+      keywords='silva app redirect permanent http',
       author='Infrae',
       author_email='info@infrae.com',
       url='',
       license='BSD',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['silva', 'silva.core'],
+      namespace_packages=['silva', 'silva.app'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'five.grok',
-          'martian >= 0.11',
-          'zope.interface',
-          'zope.schema',
-          'zope.configuration',
           'silva.core.interfaces',
-          'silva.core.services',
+          'Products.Silva',
           'zope.testing',
           ],
       )
